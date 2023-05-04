255821a6860d24234997ce95f000293731844e4f
MrGians <gianlumura97@gmail.com>
2023-05-05 00:46:47 +0200

    404 error handling with redirection & feedback

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -46,5 +47,19 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+       
+        // Custom redirect instead 404 page
+        if($e instanceof ModelNotFoundException) {
+            
+            return redirect()->route('artists.index')
+            ->with('message', 'Oops! The resource you requested does not exist..')
+            ->with('type', 'danger');
+        }
+
+        return parent::render($request, $e);
     }
 }
