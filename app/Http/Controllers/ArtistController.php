ed9ee89cbb5405394abb0e11aef751b5b3cc2246
MrGians <gianlumura97@gmail.com>
2023-05-04 21:36:51 +0200

    added artists controller

---
+++ app/Http/Controllers/ArtistController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Artist;
+use Illuminate\Http\Request;
+
+class ArtistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $artists = Artist::all();
+        return view('artists.index', compact('artists'));
+    }
+}
