43974d31e8665442738f1d8caef9224fdefc45b7
MrGians <gianlumura97@gmail.com>
2023-05-05 12:17:06 +0200

    added custom songs to data list

--- config/data/artists_songs_list.php
+++ config/data/artists_songs_list.php
@@ -32,5 +32,54 @@ return [
       'genre' => 'Female',
     ],
   ],
-  
+  'songs' => [
+    [
+      'title' => 'All Around The World',
+      'publication_date' => '2003-06-25',
+    ],
+    [
+      'title' => 'Ballad Of A Thin Man',
+      'publication_date' => '2003-11-21',
+    ],
+    [
+      'title' => 'City Of New Orleans',
+      'publication_date' => '2007-02-05',
+    ],
+    [
+      'title' => 'Don\'t Let The Stars Get In Your Eyes',
+      'publication_date' => '2007-08-11',
+    ],
+    [
+      'title' => 'Don\'t Worry, Be Happy',
+      'publication_date' => '2009-05-19',
+    ],
+    [
+      'title' => 'Let The Rest Of The World Go By',
+      'publication_date' => '2003-01-30',
+    ],
+    [
+      'title' => 'Oo-Bla-Dee (In The Land Of Oo-Bla-Dee)',
+      'publication_date' => '2005-03-18',
+    ],
+    [
+      'title' => 'Seventh Son',
+      'publication_date' => '2005-04-09',
+    ],
+    [
+      'title' => 'Star Trek Theme',
+      'publication_date' => '2011-07-27',
+    ],
+    [
+      'title' => 'Walkin\' After Midnight',
+      'publication_date' => '2009-12-21',
+    ],
+    [
+      'title' => 'Welcome To The Jungle',
+      'publication_date' => '2003-06-25',
+    ],
+    [
+      'title' => 'Cheek To Cheek',
+      'publication_date' => '2003-06-25',
+    ],
+  ]
 ];
