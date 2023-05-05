498b5d531208dd93f5f4571e0035427a6f5dce41
MrGians <gianlumura97@gmail.com>
2023-05-05 16:22:40 +0200

    added discography to single artist page

--- resources/views/artists/show.blade.php
+++ resources/views/artists/show.blade.php
@@ -32,10 +32,41 @@
                 <strong class="badge @if($artist->genre === 'Male') bg-primary @elseif($artist->genre === 'Female') bg-danger @else bg-secondary @endif bg-opacity-50">{{ $artist->genre }}</strong>
               </p>
               <p class="card-subtitle">Biography: <small class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil in minus, deserunt ex necessitatibus atque voluptatum veniam? Magni debitis quibusdam maiores inventore iusto magnam illo odit itaque, quo labore fuga.</small></p>
-              {{-- 
-              <h5 class="card-title">Artist Songs</h5>
+              
+              <h5 class="card-title mt-4">Discography</h5>
               <hr class="border @if($artist->genre === 'Male') border-primary @elseif($artist->genre === 'Female') border-danger @else border-secondary @endif border-2 opacity-50">
-               --}}
+              <ul class="list-group">
+                @forelse ($artist->songs as $song)
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="d-flex flex-column justify-content-center">
+                      <strong>{{ $song->title }}</strong>
+                      @if(count($song->artists) > 1)
+                        <small>Other Artists: 
+                          @foreach ($song->artists as $song_artist)
+                            @continue($artist->stage_name == $song_artist->stage_name)
+                            <span class="text-muted">{{ $song_artist->stage_name }}@if($loop->last).@else, @endif</span>
+                          @endforeach
+                        </small>
+                      @endif
+                    </div>
+                    <div class="w-max mx-3">   
+                      <a class="btn btn-sm btn-warning me-1" href="{{ route('songs.edit', $song) }}">
+                        <i class="fa-solid fa-pencil"></i>
+                      </a>
+
+                      <form action="{{ route('songs.destroy', $song) }}" method="POST" class="d-inline-block delete-form">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-sm btn-danger me-1" type="submit">
+                          <i class="fa-solid fa-trash"></i>
+                        </button>
+                      </form>
+                    </div>
+                  </li>
+                @empty
+                  <li class="list-group-item">This artist has no songs yet</li>
+                @endforelse
+              </ul>
             </div>
           </div>
         </div>
