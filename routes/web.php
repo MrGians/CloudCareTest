6df2a6b59d4fe6c43c812b9804a708d0a0ea7855
MrGians <gianlumura97@gmail.com>
2023-05-05 12:45:22 +0200

    added songs routes + fix comments

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\SongController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,28 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+// Artists Routes
 Route::get('/artists', [ArtistController::class, 'index'])->name('artists.index');
 Route::get('/artists/create', [ArtistController::class, 'create'])->name('artists.create');
 Route::get('/artists/{artist}', [ArtistController::class, 'show'])->name('artists.show');
 Route::post('/artists', [ArtistController::class, 'store'])->name('artists.store');
+/* Short alternative:
+    Route::resource('artists', ArtistController::class)->except(['edit', 'update', 'destroy']);
+*/
+
+// Songs Routes
+Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
+Route::get('/songs/create', [SongController::class, 'create'])->name('songs.create');
+Route::post('/songs', [SongController::class, 'store'])->name('songs.store');
+Route::get('/songs/{song}/edit', [SongController::class, 'edit'])->name('songs.edit');
+Route::put('/songs/{song}', [SongController::class, 'update'])->name('songs.update');
+Route::delete('/songs/{song}', [SongController::class, 'destroy'])->name('songs.destroy');
+/* Short alternative:
+    Route::resource('songs', SongController::class)->except('show');
+*/
 
 
+// Redirect for all others pages
 Route::get('/{any?}', function () {
     return redirect()->route('home');
 })->where('any', '.*');
